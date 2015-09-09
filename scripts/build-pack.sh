4f7b9aeeccd4e3eaca2531640772b5778838e7fe
hzsunshx <hzsunshx@corp.netease.com>
2015-09-09 20:55:22 +0800

    fix build script

--- scripts/build-pack.sh
+++ scripts/build-pack.sh
@@ -5,6 +5,12 @@ set -x
 echo $TRAVIS_GO_VERSION
 test X${GR_VERSION:-$TRAVIS_GO_VERSION} != X${TRAVIS_GO_VERSION} && exit 0
 
+if test -n "$TRAVIS"
+then
+	go get github.com/mitchellh/gox
+	gox -os="linux darwin windows" -build-toolchain
+fi
+
 set -eu
 
 TARGETDIR=gorelease-temp/dist
