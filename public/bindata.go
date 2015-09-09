0a25b9338f6539bc7e44f5435f238e1be0fd457a
hzsunshx <hzsunshx@corp.netease.com>
2015-09-09 13:33:50 +0800

    add powered link

--- public/bindata.go
+++ public/bindata.go
@@ -84,7 +84,7 @@ func bindataGo() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(420), modTime: time.Unix(1441776394, 0)}
+	info := bindataFileInfo{name: "bindata.go", size: 0, mode: os.FileMode(420), modTime: time.Unix(1441776820, 0)}
 	a := &asset{bytes: bytes, info:  info}
 	return a, nil
 }
