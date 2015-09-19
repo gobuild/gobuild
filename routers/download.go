c6790f6fecea756208f7cedf580e7367c81f718f
shengxiang <hzsunshx@corp.netease.com>
2015-09-19 12:54:46 +0800

    clean code

--- routers/download.go
+++ routers/download.go
@@ -13,27 +13,16 @@ func DownloadStats(ctx *macaron.Context, r *http.Request) {
 	_ = branch
 	repo := org + "/" + name
 
-	domain := rdx.Get("domain:" + repo).Val()
-	if domain == "" {
-		ctx.Error(405, "repo not registed in gorelease, not open register for now")
-		return
-	}
+	/*
+		domain := rdx.Get("domain:" + repo).Val()
+		if domain == "" {
+			ctx.Error(405, "repo not registed in gorelease, not open register for now")
+			return
+		}
+	*/
 
 	osarch := ctx.Params(":os") + "-" + ctx.Params(":arch")
 	rdx.Incr("downloads:" + repo)
 	rdx.Incr("downloads:" + repo + ":" + osarch)
 	ctx.JSON(200, "update success")
-	/*
-		realURL := goutils.StrFormat("http://{domain}/gorelease/{name}/{branch}/{osarch}/{name}",
-			map[string]interface{}{
-				"domain": domain,
-				"name":   name,
-				"branch": branch,
-				"osarch": osarch,
-			})
-		if ctx.Params(":os") == "windows" {
-			realURL += ".exe"
-		}
-		ctx.Redirect(realURL, 302)
-	*/
 }
